fe390cd45865c2c00b55f97b13934ff5d774d875
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-11-18 19:11:05 +0300

    fix: 1

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,8 +4,11 @@ import {Main} from "../features/main/Main";
 import {Header} from "../features/header/Header";
 import {useAppDispatch, useAppSelector} from "./hooks";
 import {initializeAppTC} from "./app-reducer";
+import {Navigate} from "react-router-dom";
+import {PATH} from "../utils/routes/routes";
 
 const App = () => {
+    const isRegisterIn = useAppSelector(state => state.auth.isRegisterIn)
     const isInitialized = useAppSelector((state) => state.app.isInitialized)
     const dispatch = useAppDispatch()
 
@@ -13,7 +16,9 @@ const App = () => {
         dispatch(initializeAppTC())
     }, [])
 
-
+if (!isRegisterIn){
+    return <Navigate to={PATH.registration}/>
+}
 if (!isInitialized) {
     return <div>Loading...</div>}
 
