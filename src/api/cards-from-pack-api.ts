fc7e303551f96df8e985133ef6f9c8f9ad22d16f
breznovic <slymarkel@gmail.com>
2022-11-21 20:20:10 +0300

    fix: cards requests

--- src/api/cards-from-pack-api.ts
+++ src/api/cards-from-pack-api.ts
@@ -1,3 +1,5 @@
+import {instance} from './cards-api'
+
 export type CardsType = {
     cardAnswer?: string
     cardQuestion?: string
@@ -31,4 +33,53 @@ export type UpdateCardType = {
     questionImg?: string
     questionVideo?: string
     answerVideo?: string
+}
+
+export type CardResponseType = {
+    answer: string
+    question: string
+    cardsPack_id: string
+    grade: number
+    shots: number
+    type: string
+    user_id: string
+    created: string
+    updated: string
+}
+
+export type CardsResponseType = {
+    cards: CardResponseType[]
+    cardsTotalCount: number
+    maxGrade: number
+    minGrade: number
+    page: number
+    pageCount: number
+    packUserId: string
+}
+
+export const cardsApi = {
+    getCards(payload = {} as CardsType) {
+        return instance.get<CardsResponseType>("cards/card", {
+            params: {
+                ...payload,
+            },
+        })
+    },
+    sendCard(payload = {} as CardRequestType) {
+        return instance.post("cards/card", {
+            card: {
+                ...payload,
+            },
+        })
+    },
+    deleteCard(id: string) {
+        return instance.delete(`cards/card?id=${id}`);
+    },
+    updateCard(payload = {} as UpdateCardType) {
+        return instance.put(`cards/card`, {
+            card: {
+                ...payload,
+            },
+        })
+    },
 }
