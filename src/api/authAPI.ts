4201d363bc68989abd255eb2fe92d67f218960bc
IshniyazovFail <filimen@mail.ru>
2022-11-24 09:55:48 +0300

    fix: filter

--- src/api/authAPI.ts
+++ src/api/authAPI.ts
@@ -1,6 +1,11 @@
 import axios from "axios"
 import {RegisterType} from '../features/registration/registration-reducer'
-import { instance } from "./instance"
+
+export const instance = axios.create({
+    // baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/' ,
+    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:7542/2.0/' : 'https://neko-back.herokuapp.com/2.0/',
+    withCredentials: true,
+})
 
 
 export const authAPI = {
