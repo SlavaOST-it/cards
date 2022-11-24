b6ddf89fe01f47798f662def1d42e1f65904f233
breznovic <slymarkel@gmail.com>
2022-11-24 13:35:45 +0300

    fix: cards reducer scratch without typification problems

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,16 +1,16 @@
-import {applyMiddleware, combineReducers, legacy_createStore} from "redux";
+import {applyMiddleware, combineReducers, legacy_createStore} from 'redux'
 import thunkMiddleware, {ThunkAction, ThunkDispatch} from 'redux-thunk'
-import {AppActionType, appReducer} from "./app-reducer";
-import {authReducer, LoginActionType} from "../features/login/auth-reducer";
+import {AppActionType, appReducer} from './app-reducer'
+import {authReducer, LoginActionType} from '../features/login/auth-reducer'
 import {registerReducer} from '../features/registration/registration-reducer'
-import {ProfileActionsType, profileReducer} from "../features/profile/profile-reducer";
-import {PassRecoveryActionType, passRecoveryReducer} from "../features/passwordRecovery/passRecovery-reducer";
-import {NewPassReducerActionType, setNewPassReducer} from "../features/newPassword/newPass-reducer";
-import {cardsReducer} from '../features/cards/cards-reducer'
+import {ProfileActionsType, profileReducer} from '../features/profile/profile-reducer'
+import {PassRecoveryActionType, passRecoveryReducer} from '../features/passwordRecovery/passRecovery-reducer'
+import {NewPassReducerActionType, setNewPassReducer} from '../features/newPassword/newPass-reducer'
+import {CardsActionsType, cardsReducer} from '../features/cards/cards-reducer'
 
 const rootReducer = combineReducers({
     app: appReducer,
-    login:authReducer,
+    login: authReducer,
     auth: registerReducer,
     profile: profileReducer,
     passRecovery: passRecoveryReducer,
@@ -19,11 +19,11 @@ const rootReducer = combineReducers({
 })
 
 // ===== Принимаем типизацию всех редьюсеров ===== //
-type ReduxActionType = AppActionType|LoginActionType|ProfileActionsType | PassRecoveryActionType | NewPassReducerActionType
-
+type ReduxActionType = AppActionType | LoginActionType | ProfileActionsType |
+    PassRecoveryActionType | NewPassReducerActionType | CardsActionsType
 
 export type AppStateType = ReturnType<typeof rootReducer>
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunkMiddleware));
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunkMiddleware))
 export type AppRootStateType = ReturnType<typeof rootReducer>
 export type RootState = ReturnType<typeof store.getState>
 
@@ -33,4 +33,4 @@ export type AppDispatchType = ThunkDispatch<RootState, unknown, ReduxActionType>
 // ===== Типизация того что возвращает нам Thunk ===== //
 export type AppThunkType<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, ReduxActionType>
 // @ts-ignore
-window.store = store;
+window.store = store
