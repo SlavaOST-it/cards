48a236b4f5e8a1467c85c75204048c84fd3c859f
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-12-12 00:43:59 +0300

    fix: rename path

--- src/utils/routes/routes.ts
+++ src/utils/routes/routes.ts
@@ -5,6 +5,6 @@ export const PATH = {
     error: '/error',
     passwordRecovery: '/passwordRecovery',
     setNewPassword: '/setNewPass/:token',
-    packList: "/cards",
+    packList: "/packs",
     cardList: "/cards"
 }
