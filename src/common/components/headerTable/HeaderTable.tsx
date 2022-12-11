c2eebd71b1a62aa3b559f7465c095607d3bb1701
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2022-12-12 00:47:56 +0300

    fix: change css style, add disable button

--- src/common/components/headerTable/HeaderTable.tsx
+++ src/common/components/headerTable/HeaderTable.tsx
@@ -1,19 +1,21 @@
 import React, {FC} from 'react';
-import style from "../../../features/cards/PacksList.module.css";
+import style from "./HeaderTable.module.css";
 import Button from "@mui/material/Button";
 
 type HeaderType={
     callbackToAdd:()=>void
     title?:string
     titleButton: string
+    disabled?: boolean
 }
 
-export const HeaderTable:FC<HeaderType> = ({callbackToAdd, title,titleButton}) => {
+export const HeaderTable:FC<HeaderType> = ({callbackToAdd, title,titleButton, disabled}) => {
     return (
         <div className={style.header}>
             <h2>{title}</h2>
             <div>
                 <Button
+                    disabled={disabled}
                     onClick={callbackToAdd}
                     sx={{borderRadius: 5}} size="small"
                     variant="contained"
